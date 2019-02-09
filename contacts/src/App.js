b8654bf995eb47b1864139e757c4deefbeba4fc6
Pedro Neves <pedro.hfn7@gmail.com>
2019-02-09 12:20:19 -0300

    Testing class components

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends Component {
+	render () {
+		const { contacts = [] } = this.props;
+
+		if (!contacts.length) {
+			return (<ul>
+				<li>No contacts right now :(</li>
+			</ul>);
+		}
+
+		return (
+			<ol>{
+				contacts.map((contact, i) => {
+					return (
+						<li key={i}>{contact}</li>
+					)
+				})
+			}</ol>
+		)
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<ContactList contacts={['Pedro', 'João', 'Maria']} />
+				<ContactList contacts={['Pedro']} />
+				<ContactList />
+			</div>
+		);
+	}
 }
 
 export default App;
