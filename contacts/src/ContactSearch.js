db8810e5d1a0eead07bfd193a91e696d172c245f
Pedro Neves <pedro.hfn7@gmail.com>
2019-02-16 14:44:19 -0300

    Adding state to ContactSearch component

--- contacts/src/ContactSearch.js
+++ contacts/src/ContactSearch.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 
 class ContactSearch extends Component {
-    render () {
-        return (
-            <div>
-                <input type="text" placeholder="Click here to filter your contacts" />
-            </div>
-        )
-    }
+	constructor (props) {
+		super(props)
+		this.state = {
+			query: ''
+		}
+	}
+
+	updateQuery (query) {
+		console.log(query.trim())
+		this.setState(() => ({ query }));
+	}
+
+	render () {
+		return (
+			<div>
+				<input
+					type="text"
+					placeholder="Click here to filter your contacts"
+					value={this.state.query}
+					onChange={event => this.updateQuery(event.target.value)}
+				/>
+			</div>
+		)
+	}
 }
 
 export default ContactSearch;
