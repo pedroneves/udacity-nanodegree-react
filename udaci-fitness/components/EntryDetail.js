4734f823a0902eaf9dd982f481fa6b879636d0f6
Pedro Neves <pedro.hfn7@gmail.com>
2019-04-29 12:27:18 -0300

    Adding content to MetricsCard

--- udaci-fitness/components/EntryDetail.js
+++ udaci-fitness/components/EntryDetail.js
@@ -1,14 +1,46 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { View, Text, StyleSheet } from 'react-native';
+import * as Colors from '../utils/colors';
+
+import MetricCard from './MetricCard';
 
 class EntryDetail extends Component {
+	static navigationOptions = ({ navigation }) => {
+		const { entryId } = navigation.state.params;
+
+		const year = entryId.slice(0,4)
+		const month = entryId.slice(5,7)
+		const day = entryId.slice(8)
+
+		return {
+			title: `${day}/${month}/${year}`
+		}
+	}
+
 	render() {
+		const { metrics } = this.props;
+
 		return (
-			<View>
-				<Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
+			<View style={styles.container}>
+				<MetricCard metrics={metrics} />
 			</View>
 		)
 	}
 }
 
-export default EntryDetail;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: Colors.white,
+		padding: 15
+	}
+});
+
+function mapStateToProps (state, { navigation }) {
+	const { entryId } = navigation.state.params;
+	const metrics = state[entryId];
+	return { entryId, metrics }
+}
+
+export default connect(mapStateToProps)(EntryDetail);
